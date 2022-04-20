19a329883565170353c6f75e6415aa9bee3504d9
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-04-20 15:28:58 +0200

    consegna

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 // alert("Hello World!")
 
-const app = new Vue({
-  el: '#id',
-  data: {
+// nome repo: vue-hello
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+// Buon lavoro e buon divertimento con Vue !!!! 
 
+
+
+const app = new Vue({
+  el: '#root',
+  data: {
+    "title": "Hello World!",
+    "image": "https://unsplash.it/300/300?image=10"
   },
   methods:{
 
